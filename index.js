15a7bef827cb352bf5340b41e81e025384b5e4e5
+Michael Kolb <+michael.w.kolb@gmail.com>
2022-01-14 17:56:49 -0500

    commit tree 287c63961a9baab321f057ff236ae75ea42bfbc4

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const timeIn = [];
-const timeOut = [];
+
 
 function createEmployeeRecord(array) {
-        
+        const timeIn = [];
+        const timeOut = [];
         const empRecord = Object.create({});
         empRecord.firstName = array[0];
         empRecord.familyName = array[1];
@@ -10,60 +10,95 @@ function createEmployeeRecord(array) {
         empRecord.payPerHour = array[3];
         empRecord.timeInEvents = timeIn;
         empRecord.timeOutEvents = timeOut;
-        // console.log(empRecord);
         return empRecord;
 };
 
 
 function createEmployeeRecords(cersArray) {
     const empIndex = [];
-    // console.log(cersArray);
     for (const element of cersArray) {
-        // console.log(element);
     const employeeRecord = createEmployeeRecord(element);
     empIndex.push(employeeRecord);
     };
-    // console.log(empIndex);
     return empIndex;
 };
 
 function createTimeInEvent(empRec, stamp) {
-    console.log(empRec);
-    let timeIn = stamp.split('');
+    let timeStamp = stamp.split('');
     const dateArray = [];
     const timeArray = [];
-    for (let i = 0; i < timeIn.length; i++) {
-        if (i < timeIn.length - 5) {
-            dateArray.push(timeIn[i]);
+    for (let i = 0; i < timeStamp.length; i++) {
+        if (i < timeStamp.length - 5) {
+            dateArray.push(timeStamp[i]);
         }
-        else if (timeIn[i] === ' ') {
+        else if (timeStamp[i] === ' ') {
         }
         else {
-            timeArray.push(timeIn[i]);
+            timeArray.push(timeStamp[i]);
         };
     }
-    timeIn = JSON.stringify(timeArray);
-    
-    console.log(timeIn);
+    let tempTime = timeArray.join('');
+    let tempDate = dateArray.join('');
+    let timeIn = Object.create({});
+    timeIn.type = 'TimeIn';
+    timeIn.hour = parseInt(tempTime);
+    timeIn.date = tempDate;
+    empRec.timeInEvents.push(timeIn);
     return empRec;
 };
 
-function createTimeOutEvent() {
-    
+function createTimeOutEvent(empRec, stamp) {
+    let timeStamp = stamp.split('');
+    const dateArray = [];
+    const timeArray = [];
+    for (let i = 0; i < timeStamp.length; i++) {
+        if (i < timeStamp.length - 5) {
+            dateArray.push(timeStamp[i]);
+        }
+        else if (timeStamp[i] === ' ') {
+        }
+        else {
+            timeArray.push(timeStamp[i]);
+        };
+    }
+    let tempTime = timeArray.join('');
+    let tempDate = dateArray.join('');
+    let timeOut = Object.create({});
+    timeOut.type = 'TimeOut';
+    timeOut.hour = parseInt(tempTime);
+    timeOut.date = tempDate;
+    empRec.timeOutEvents.push(timeOut);
+    return empRec;
 };
 
-function hoursWorkedOnDate() {
-
+function hoursWorkedOnDate(empRec, date) {
+    if (date === empRec.timeOutEvents[0].date && empRec.timeInEvents[0].date) {
+        let hoursWorked = empRec.timeOutEvents[0].hour - empRec.timeInEvents[0].hour;
+        return hoursWorked / 100;
+    }
 };
 
-function wagesEarnedOnDate() {
-
+function wagesEarnedOnDate(empRec, date) {
+    let empHours = hoursWorkedOnDate(empRec, date);
+    let tempHourly = empRec.payPerHour;
+    let payOwed = empHours * tempHourly;
+    return payOwed;
 };
 
-function allWagesFor() {
-
+function allWagesFor(empRec) {
+    let totalHours = 0;
+    for (let i=0; i < empRec.timeInEvents.length; i++) {
+        let hoursWorked = (empRec.timeOutEvents[i].hour - empRec.timeInEvents[i].hour) / 100;
+        totalHours = totalHours + hoursWorked;
+    }
+    let totalOwed = totalHours * empRec.payPerHour;
+    return totalOwed;
 };
 
-function calculatePayroll() {
-
+function calculatePayroll(empRecs) {
+    let totalPayroll = 0;
+    for (let emp of empRecs) {
+        totalPayroll += allWagesFor(emp);
+    }
+    return totalPayroll;
 };
